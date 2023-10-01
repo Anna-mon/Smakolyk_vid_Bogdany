54f80bcb78517af5b7fe915a5ef365f54592c150
Anna-mon <61828974+Anna-mon@users.noreply.github.com>
2023-10-01 20:01:04 +0300

    Add files via upload

---
+++ anna.js
@@ -0,0 +1,11 @@
+import { createClient } from 'pexels';
+
+const client = createClient('3NuPyZQ45ZAddSDkuSgdgmSpxa49Oahaq1j7K7IomLNOBjd8Futx7Bla');
+
+client.photos.curated({ per_page: 1 }).then(photos => {www.smakolyk.com});
+
+
+
+
+
+  
